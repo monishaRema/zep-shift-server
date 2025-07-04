ba986762a89430020cf14bfdb83abde0b34fb24c
monisharema <monisharema97@gmail.com>
2025-07-04 22:40:15 +0300

    fetch user from search and make admin or remove api created

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
+const { verifyFBToken } = require("../middlewares/verifyFBToken");
 
 module.exports = ({ userCollection }) => {
   const router = express.Router();
@@ -34,6 +36,51 @@ module.exports = ({ userCollection }) => {
       res.status(500).send({ message: "Server error", insertedId: false });
     }
   });
+
+  // GET : user depending on search
+router.get("/search",verifyFBToken, async (req, res) => {
+  const emailQuery = req.query.email;
+
+  if (!emailQuery) return res.status(400).json({ message: "Email query required" });
+  const safeQuery = emailQuery.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  try {
+    const users = await userCollection
+      .find({ email: { $regex: safeQuery, $options: "i" } })
+      .limit(10)
+      .toArray();
+
+    res.json(users);
+  } catch (error) {
+    console.error("Search error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.patch("/:id/role", verifyFBToken, async (req, res) => {
+  const { id } = req.params;
+  const { role } = req.body;
+
+  if (!["admin", "user"].includes(role)) {
+    return res.status(400).json({ message: "Invalid role" });
+  }
+
+  try {
+    const result = await userCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { role } }
+    );
+
+    res.json({
+      message: `User role updated to ${role}`,
+      modifiedCount: result.modifiedCount,
+    });
+  } catch (error) {
+    console.error("Role update error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
 
   return router;
 };
